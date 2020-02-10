cc958b176c44d4f68a9e2b947bcb5850ee3af538
Ashok <ashokvmit@gmail.com>
2020-02-10 23:00:21 +0530

    loo

---
+++ ForSoftBank.php
@@ -0,0 +1,37 @@
+<?php
+?>
+
+
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="softBank.css">
+</head>
+<title>HOME</title>
+
+
+<body>
+
+<div id="main_div">
+		<div class="vertical-menu">
+              <a class="First_Buttons" href="#">Link 0</a>
+              		
+              		<div>
+              
+                      <a id="button_two" href="#">Sub 1</a>
+                      <a id="button_two" href="#">Sub 3</a>
+                      <a id="button_two" href="#">Sub 4</a>
+                      
+                     </div>
+          
+              <a class="First_Buttons"href="#">Link 1</a>
+              <a class="First_Buttons" href="#">Link 2</a>
+              <a class="First_Buttons" href="#">Link 3</a>
+              <a class="First_Buttons" href="#">Link 4</a>
+        </div>
+
+</div>
+
+</body>
+
+
+</html>
